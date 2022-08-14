ba712701f4ad8c87132e8bf2137f905de060c288
peshwar9 <par.esh1937@gmail.com>
2022-08-14 06:38:06 +0530

    fetch remote api works

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use yew::prelude::*;
+use reqwasm::http::Request;
 mod video;
 mod video_detail;
 use crate::video::*;
@@ -18,49 +19,37 @@ fn app() -> Html {
         <VideoDetails video={video.clone()} />
     });
 
-    let videos = vec![
-        Video {
-            id: 1,
-            title: "Bhagavad Gita Sahasragala parayana".to_string(),
-            speaker: "HH Sri Ganapathy Sachchidananda Swamiji".to_string(),
-            url: "www.dattapeetham.org".to_string(),
-        },
-        Video {
-            id: 2,
-            title: "Building and breaking things".to_string(),
-            speaker: "John Doe".to_string(),
-            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
-        },
-        Video {
-            id: 3,
-            title: "The development process".to_string(),
-            speaker: "Jane Smith".to_string(),
-            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
-        },
-        Video {
-            id: 4,
-            title: "The Web 7.0".to_string(),
-            speaker: "Matt Miller".to_string(),
-            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
-        },
-        Video {
-            id: 5,
-            title: "Mouseless development".to_string(),
-            speaker: "Tom Jerry".to_string(),
-            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
-        },
-    ];
+    //
+    let videos = use_state(|| vec![]);
+        {
+            let videos = videos.clone();
+            use_effect_with_deps(move |_| {
+                let videos = videos.clone();
+                wasm_bindgen_futures::spawn_local(async move {
+                    let fetched_videos: Vec<Video> = Request::get("/tutorial/data.json")
+                        .send()
+                        .await
+                        .unwrap()
+                        .json()
+                        .await
+                        .unwrap();
+                    videos.set(fetched_videos);
+                });
+                || ()
+            }, ());
+        }
+
+
+    //
+        let details = details.clone();
     html! {
         <>
         <div>
         <h3>{"Videos to watch"}</h3>
-            <VideosList videos={videos} on_click={on_video_select.clone()} />
+            <VideosList videos={(*videos).clone()} on_click={on_video_select.clone()} />
         </div>
         {for details}
-        <div>
-            <h3>{ "JGD. This is first static page with Yew and Wasm" }</h3>
-            <img src="https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder" alt="video thumbnail" />
-        </div>
+
         </>
     }
 }
